8aee9074298424ebd41730f60dba57968f504358
Emily Martinez <em3021@columbia.edu>
2016-05-21 19:11:14 -0400

    adds mobile styles, updates blurring

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ $(window).scroll(function() {
 
 var posterHeight = $(".poster").outerHeight();
 
-if ($(this).scrollTop() > (posterHeight - 100)){  
-    $('.background').addClass("background-blur");
+if ($(this).scrollTop() > (posterHeight - 200)){  
+    $('body').addClass("background-blur");
   }
   else {
-    $('.background').removeClass("background-blur");
+    $('body').removeClass("background-blur");
   }
 });
